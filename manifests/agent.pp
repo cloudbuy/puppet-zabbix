47a2327a186bad75a947448474fecc75a1e7bc08
Damien Churchill <damoxc@gmail.com>
2016-08-18 09:34:56 +0100

    add a check for Windows before trying to remove /etc/zabbix/zabbix_agentd.conf

--- manifests/agent.pp
+++ manifests/agent.pp
@@ -319,7 +319,7 @@ class zabbix::agent (
     require => Package[$zabbix_package_agent],
   }
 
-  if $agent_configfile_path != '/etc/zabbix/zabbix_agentd.conf' {
+  if ($agent_configfile_path != '/etc/zabbix/zabbix_agentd.conf') and ($::kernel != 'windows') {
     file { '/etc/zabbix/zabbix_agentd.conf':
       ensure  => absent,
       require => Package[$zabbix_package_agent],
